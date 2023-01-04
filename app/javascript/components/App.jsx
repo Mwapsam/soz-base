2b0ef426004e624e75769a8d264708d95bec5ac3
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:16:59 +0200

    Add all pages routes

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,25 +1,30 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Signup, Login, Dashboard, Wrapper, Products, List, Show, Cart } from '../pages/index';
-import { Success } from '../components';
+import { Routes, Route } from 'react-router-dom';
+import { Signup, Login, Dashboard, Wrapper, Products, List, Show, Cart, Checkout, Admin, Users, Sales, AdminProducts } from '../pages/index';
 
 const App = () => {
-  const DashboardComponent = Wrapper(Dashboard)
-  const ProductsComponent = Wrapper(Products)
+  const ProductsComponent = Admin(Products)
+  const SalesComponent = Admin(Sales)
+  const UsersComponent = Admin(Users)
+  const AdminProductsComponent = Admin(AdminProducts)
   const ListComponent = Wrapper(List)
   const ShowComponent = Wrapper(Show)
   const CartComponent = Wrapper(Cart)
-  const SuccessComponent = Wrapper(Success)
+  const CheckoutComponent = Wrapper(Checkout)
+  
   return (
     <Routes>
         <Route path='/register' element={<Signup/>} />
         <Route path='/login' element={<Login />} />
-        <Route path='/' element={<DashboardComponent />} />
+        <Route path='/' element={<Dashboard />} />
         <Route path='/products' element={<ProductsComponent/>} />
         <Route path='/products/:product' element={<ShowComponent/>} />
         <Route path='/list' element={<ListComponent/>} />
         <Route path='/cart' element={<CartComponent />} />
-        <Route path='/success?session_id/:session_id' element={<SuccessComponent />} />
+        <Route path='/checkout' element={<CheckoutComponent />} />
+        <Route path='/sales' element={<SalesComponent />} />
+        <Route path='/users' element={<UsersComponent />} />
+        <Route path='/admin-products' element={<AdminProductsComponent />} />
     </Routes>
   );
 };
