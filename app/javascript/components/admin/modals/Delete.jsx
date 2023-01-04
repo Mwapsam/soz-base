7f325044d20d2ba617a1470efab7e2cccbaf31a2
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:23:35 +0200

    Create delete modal component

---
+++ app/javascript/components/admin/modals/Delete.jsx
@@ -0,0 +1,44 @@
+import React, { Fragment } from "react";
+import {
+  Button,
+  Dialog,
+  DialogHeader,
+  DialogBody,
+  DialogFooter,
+} from "@material-tailwind/react";
+
+const Delete = ({openDelete, handleOpenDelete, productName}) => {
+    console.log(productName);
+  return (
+    <>  
+    <Fragment>
+      <Dialog
+        open={openDelete}
+        handler={handleOpenDelete}
+        animate={{
+          mount: { scale: 1, y: 0 },
+          unmount: { scale: 0.9, y: -100 },
+        }}
+      >
+        <DialogHeader className="text-xl mb-4 font-bold text-black text-center">Do you Want Delete {productName}?</DialogHeader>
+        <DialogFooter>
+          <Button
+            variant="text"
+            color="red"
+            onClick={handleOpenDelete}
+            className="mr-1"
+          >
+            <span>Cancel</span>
+          </Button>
+          <Button variant="gradient" color="gray" onClick={handleOpenDelete}>
+            <span>Confirm</span>
+          </Button>
+        </DialogFooter>
+      </Dialog>
+    </Fragment>
+    
+    </>
+  )
+}
+
+export default Delete;
