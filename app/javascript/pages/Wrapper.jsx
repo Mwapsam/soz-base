1208711e4003cc4b428cbb638226953dee01f231
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:28:09 +0200

    Create a wrapper HOF

--- app/javascript/pages/Wrapper.jsx
+++ app/javascript/pages/Wrapper.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import NavBar from '../components/navigation/Navbar';
+import Footer from '../components/navigation/Footer';
 
 const Wrapper = (Component) => ({...props}) => {
     return (
         <section>
             <NavBar />
             <Component {...props}/>
+            <Footer />
         </section>
     )
 }
