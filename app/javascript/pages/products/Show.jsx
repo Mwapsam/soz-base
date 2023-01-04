7a914617c86289ac12d785c803374c875605f5ab
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:35:40 +0200

    Create transaction hook

--- app/javascript/pages/products/Show.jsx
+++ app/javascript/pages/products/Show.jsx
@@ -1,65 +1,68 @@
-import React, {useState, useEffect} from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React from 'react';
+import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
-import { Card, CardHeader, CardBody, CardFooter, Typography, Tooltip, Button } from "@material-tailwind/react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping, faHeart } from '@fortawesome/free-solid-svg-icons';
+import { Card, CardHeader, CardBody, Button } from "@material-tailwind/react";
 import useProductFetch from '../hooks/useProductFetch';
-import { addToCart, decreaseCart, getTotals, removeFromCart } from '../../reducers/cart';
 import { addToCartFunc, removeFromCartFunc } from '../../services/cart.service';
+import { incrementFunc, decrementFunc } from '../../services/product.service';
+import { ProductDetail, Crambs, MyTabs } from '../../components';
+import useCart from '../hooks/useCart';
 
 const Show = () => {
     const products = useProductFetch();
-    const cart = useSelector((state) => state.cart);
+    const { cart } = useCart();
     const { product } = useParams();
     const dispatch = useDispatch();
 
-    useEffect(() => {
-      dispatch(getTotals());
-    }, [cart, dispatch]);
-
-    const handleCart = (product) => {
-      dispatch(addToCartFunc(product))
-    }
 
     const handleDecreaseCart = (product) => {
-      dispatch(decreaseCart(product))
+      dispatch(decrementFunc(product))
     }
 
-    const handleIncreaseCart = (product) => {
-      dispatch(addToCart(product))
+    const  handleIncreaseCart = (product) => {
+      dispatch(incrementFunc(product))
     }
 
     const handleRemoveFromCart = (product) => {
-      dispatch(removeFromCart(product));
       dispatch(removeFromCartFunc(product));
     };
-
 
     const prod = products && products.find((item) => item.id == product);
     const cartIt = cart && cart.cartItems.find((item) => item.id == product)
 
+    const handleCart = (product) => {
+        const cartData = {
+        id: product.id,
+        quantity: 1
+        }
+        dispatch(addToCartFunc(cartData))
+    }
+
   return (
-    <Card className='mx-6 my-12' shadow={false} style={{borderRadius: 0, width: '90%', height: '100vh' }}>
-        <CardHeader shadow={false} style={{borderRadius: 0}} >
-            <img src={prod && prod.photos_urls && prod?.photos_urls[0]} alt="profile-picture" />
-        </CardHeader>
-        <div className='m-4'>
-          <h3 className='font-bold'>{prod && prod.name}</h3>
-          <p className='py-1 font-semibold'>{prod && prod.price.toLocaleString('en-US', { style: 'currency', currency: prod && prod.currency })}</p>
-          <p>{prod && prod.description}</p>
-          <div className='flex flex-grow gap-6'>
-            <div className='flex justify-between items-center middle font-sans center transition-all border-2 w-32 h-10 p-4 text-black bg-white mt-2'>
-              <span className='text-xl cursor-pointer' onClick={() => handleDecreaseCart(prod)}>-</span>
-              <span className='text-lg'>{cartIt && cartIt.cartQuantity}</span>
-              <span className='text-xl cursor-pointer' onClick={() => handleIncreaseCart(prod)}>+</span>
-            </div>
-            {cartIt ? <Button style={{borderRadius: 0}} className='bg-red-900 mt-2' type='submit' onClick={() => handleRemoveFromCart(prod)}>Remove from Cart</Button> :
-            <Button style={{borderRadius: 0}} className='bg-gray-900 mt-2' type='submit' onClick={() => handleCart(prod)}>Add to Cart</Button>
-            }
-          </div>
-        </div>
-    </Card>
+    <>
+      {products.length !== 0 ? 
+        (<>
+          <Crambs />
+          <ProductDetail 
+            Card={Card} 
+            CardBody={CardBody} 
+            CardHeader={CardHeader} 
+            Button={Button} prod={prod} 
+            cartIt={cartIt} 
+            handleDecreaseCart={handleDecreaseCart}
+            handleIncreaseCart={handleIncreaseCart}
+            handleRemoveFromCart={handleRemoveFromCart}
+            handleCart={handleCart}
+          />
+          <MyTabs  
+            description={prod.description} 
+            id={prod.id} 
+          />
+        </>) :
+        <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
+      }
+    </>
+    
   )
 }
 
