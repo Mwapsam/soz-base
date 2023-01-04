202b9665b4da6937d40ba96dd29e459c830163b3
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:40:40 +0200

    Create transaction hook

--- app/javascript/store/store.js
+++ app/javascript/store/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { userSlice } from '../reducers/users';
 import { productSlice } from '../reducers/product';
+import { ordersSlice } from '../reducers/orders';
 import cartSlice from '../reducers/cart';
 import paymentSlice from '../reducers/payments';
+import addressSlice from '../reducers/address';
+import transactionSlice from '../reducers/transaction';
+import transactionsSlice from '../reducers/allTransactions';
+import allUsersSlice from '../reducers/all_users';
+import productsSlice from '../reducers/products';
 
 const store = configureStore({
   reducer: {
@@ -10,6 +16,12 @@ const store = configureStore({
     product: productSlice.reducer,
     cart: cartSlice.reducer,
     payment: paymentSlice.reducer,
+    address: addressSlice.reducer,
+    order: ordersSlice.reducer,
+    transaction: transactionSlice.reducer,
+    transactions: transactionsSlice.reducer,
+    allUsers: allUsersSlice.reducer,
+    products: productsSlice.reducer,
   },
 });
 export default store;
