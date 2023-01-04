fd6cd44603457f5de23b42efbafd989d6a7d9251
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:35:30 +0200

    Create transaction hook

--- app/javascript/pages/products/List.jsx
+++ app/javascript/pages/products/List.jsx
@@ -1,42 +1,50 @@
 import React, {useState} from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Card, CardHeader, Tooltip, Select, Option } from "@material-tailwind/react";
+import { Card, CardHeader, Tooltip } from "@material-tailwind/react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCartShopping, faHeart, faList, faSliders, faMagnifyingGlass, faBarcode, faBraille } from '@fortawesome/free-solid-svg-icons';
+import { faCartShopping, faHeart, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import useProductFetch from '../hooks/useProductFetch';
+import Hero from '../../components/header/Hero';
+import { addToCartFunc } from '../../services/cart.service';
+import useCart from '../hooks/useCart';
 
 const List = () => {
   const [toggle, setToggle] = useState(false);
   const products = useProductFetch();
+  const { cart } = useCart();
   const [searchTerm, setSearchTerm] = useState('');
+  const dispatch = useDispatch();
 
   const productFilter = (e) => {
     setSearchTerm(e.target.value.toLowerCase());
   };
 
+  console.log(cart);
+
   const handleToggle = (e) => {
     setToggle((prev) => !prev)
   }
+
+  const handleCart = (product) => {
+    const cartData = {
+      id: product.id,
+      quantity: 1
+    }
+    dispatch(addToCartFunc(cartData))
+  }
+
   return (
     <div className='flex flex-col'>
-      <div className='flex justify-end gap-3 mr-6'>
-        <span className='my-3 font-bold'>Filter</span>
-        <FontAwesomeIcon icon={faSliders} className='my-4' />
-        <div className="w-50 mx-5">
-          <Select variant="standard" className='hover:border-none' label="Sort by Price" style={{borderRadius: 0, border: 'none'}}>
-            <Option>Less than 1000</Option>
-            <Option>1000 to 2000</Option>
-            <Option>2000 to 3000</Option>
-            <Option>3000 to 4000</Option>
-            <Option>Above 5000</Option>
-          </Select>
-        </div>
-        {toggle ? (<div class="flex justify-center">
-          <div class="mb-3 xl:w-96">
-            <div class="input-group relative flex flex-wrap items-stretch w-full mb-4 rounded">
+      <Hero />
+      <div className='flex justify-end gap-3 mr-6 mt-4'>
+        {toggle ? (<div className="flex justify-center">
+          <div className="mb-3 xl:w-96">
+            <div className="input-group relative flex flex-wrap items-stretch w-full mb-4 rounded">
               <input type="search"
                 onMouseLeave={handleToggle} 
-                class="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                className="form-control relative flex-auto min-w-0 block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                 placeholder="Search" aria-label="Search" 
                 aria-describedby="button-addon2" 
                 value={searchTerm}
@@ -49,13 +57,9 @@ const List = () => {
           <FontAwesomeIcon icon={faMagnifyingGlass} className='my-4 mr-5' />
         </span>
         }
-        <FontAwesomeIcon icon={faList} className='my-4' />
-        
-        <FontAwesomeIcon icon={faBarcode} className='my-4' />
-        <FontAwesomeIcon icon={faBraille} className='my-4' />
       </div>
       
-      <div className='grid xs:grid-cols-1 ss:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 m-4'>
+      <div className='grid justify-center xs:grid-cols-1 ss:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 m-4'>
         {products && products
         .filter((product) => {
           if (searchTerm === '') {
@@ -64,31 +68,39 @@ const List = () => {
           return product.name.toLowerCase().includes(searchTerm);
         })
         .map((product) => (
-            <Link to={`/products/${product.id}`} key={product.id}>
-              <Card className="w-86 bg-gray-50" style={{borderRadius: 0}}>
-                  <CardHeader floated={false} shadow={false} style={{borderRadius: 0, width: '90%', height: '80%' }} className="h-80 w-100">
-                    <img src={product.photos_urls && product.photos_urls[0]} alt="profile-picture" />
-                  </CardHeader>
-                  <div className='flex justify-between p-4'>
-                    <div>
-                      <h5>{product.name}</h5>
-                      <p>{product.price.toLocaleString('en-US', {
-                          style: 'currency',
-                          currency: product.currency,
-                        })}</p>
-                    </div>
-                    <div className='flex gap-2'>
-                    <Tooltip content="Add to Cart">
-                      <p><FontAwesomeIcon icon={faCartShopping} /></p>
-                    </Tooltip>
-                    <Tooltip content="Add to Wishlist">
-                      <p><FontAwesomeIcon icon={faHeart} /></p>
-                    </Tooltip>
-                    </div>
+          <div key={product.id}>
+            <Card className="w-72 bg-gray-50" style={{borderRadius: 0}}>
+              <Link to={`/products/${product.id}`}>
+                <CardHeader floated={false} shadow={false} style={{borderRadius: 0, width: '90%', height: '80%' }} className="h-100 w-80 relative overflow-hidden bg-no-repeat bg-cover max-w-xs">
+                  <LazyLoadImage
+                    src={product.photos_urls && product.photos_urls[0]} 
+                    alt="profile-picture"  
+                    placeholderSrc={placeholder}
+                  />
+                  <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-30 transition duration-300 ease-in-out bg-red-700"></div>
+                </CardHeader>
+              </Link>
+                
+                <div className='flex justify-between p-4'>
+                  <div>
+                    <h5>{product.name}</h5>
+                    <p>{product.price.toLocaleString('en-US', {
+                        style: 'currency',
+                        currency: product.currency,
+                      })}</p>
                   </div>
-              </Card>
-            </Link>
-            
+                  <div className='flex gap-2'>
+                  
+                    {cart && cart.cartItems.find((item) => item.id === product.id) ? (<Tooltip className='bg-red-700' content="Added to Cart"><span><FontAwesomeIcon icon={faCartShopping} className='text-gray-400' /></span></Tooltip>):
+                      (<Tooltip content="Add to Cart"><span><FontAwesomeIcon icon={faCartShopping}  className='cursor-pointer' onClick={() => handleCart(product)}/></span></Tooltip>)
+                    }
+                  <Tooltip content="Add to Wishlist">
+                    <p><FontAwesomeIcon icon={faHeart} /></p>
+                  </Tooltip>
+                  </div>
+                </div>
+            </Card>
+          </div>
         ))}
       </div>
     </div>
