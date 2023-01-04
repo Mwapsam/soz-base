dab9d8ddaadf80c21fbeca828125e00a6250b4c8
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:43:02 +0200

    Create transaction hook

---
+++ spec/factories/addresses.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :address do
+    city { "MyString" }
+    country { "MyString" }
+    line1 { "MyString" }
+    line2 { "MyString" }
+    postal_code { "MyString" }
+    state { "MyString" }
+  end
+end
