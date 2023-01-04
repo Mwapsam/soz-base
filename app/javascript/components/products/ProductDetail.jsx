ad7ffc222ea4d329d56717999ca95bd809327c2a
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:27:10 +0200

    Create product details component

---
+++ app/javascript/components/products/ProductDetail.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+const ProductDetail = (
+  {Card, CardBody, CardHeader, Button, prod, cartIt, handleCart, handleDecreaseCart, handleIncreaseCart, handleRemoveFromCart }
+  ) => {
+  return (
+    <Card className='mx-6 mt-12' shadow={false} style={{ borderRadius: 0, width: '90%' }}>
+    <div className="lg:flex justify-center items-start w-full">
+      <CardHeader shadow={false} style={{ borderRadius: 0 }}>
+        <img src={prod && prod.photos_urls && prod?.photos_urls[0]} alt="profile-picture" />
+      </CardHeader>
+      <CardBody className='md:w-2/3 lg:w-3/4'>
+        <div className='m-4 w-full'>
+          <h3 className='font-bold'>{prod && prod.name}</h3>
+          <p className='py-1 font-semibold'>{prod && prod.price.toLocaleString('en-US', { style: 'currency', currency: prod && prod.currency })}</p>
+          <p style={{ width: '100%' }}>{prod && prod.description}</p>
+          <div className='flex flex-grow gap-6'>
+            <div className='flex justify-between items-center middle font-sans center transition-all border-2 w-32 h-10 p-4 text-black bg-white mt-2'>
+              {cartIt?.carts[0] ? <button className='text-xl cursor-pointer' onClick={() => handleDecreaseCart(cartIt?.orderables[0].id)}>-</button>: 
+                <button disabled className='text-xl text-gray-400'>-</button>
+              }
+              <span className='text-lg'>{ cartIt?.orderables[0] ? cartIt.orderables[0].quantity : 0 }</span>
+              {cartIt?.carts[0] ? <button className='text-xl cursor-pointer' onClick={() => handleIncreaseCart(cartIt?.orderables[0]?.id)}>+</button> : 
+                <button disabled className='text-xl text-gray-400'>+</button>
+              }
+            </div>
+            {cartIt?.carts[0] ? <Button style={{ borderRadius: 0 }} className='bg-red-900 mt-2' type='submit' onClick={() => handleRemoveFromCart(cartIt?.orderables[0].id)}>Remove from Cart</Button> :
+              <Button style={{ borderRadius: 0 }} className='bg-gray-900 mt-2' type='submit' onClick={() => handleCart(prod)}>Add to Cart</Button>}
+          </div>
+        </div>
+      </CardBody>
+    </div>
+  </Card>
+  )
+}
+
+export default ProductDetail;
