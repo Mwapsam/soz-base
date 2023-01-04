bfbee1d785a3e9db553f009ee7ed8d68fe900c5f
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:30:00 +0200

    Create admin dashboard page

--- app/javascript/pages/dashboard/Dashboard.jsx
+++ app/javascript/pages/dashboard/Dashboard.jsx
@@ -1,42 +1,46 @@
-import React, { Fragment } from 'react';
-import { Oval } from  'react-loader-spinner'
+import React from 'react';
+import Nav from '../../components/admin/Nav';
+import Sales from '../../components/admin/Sales';
+import SideBar from '../../components/admin/SideBar';
+import Table from '../../components/admin/Table';
+import Footer from '../../components/admin/Footer';
 import useUser from '../hooks/useUser';
+import useTransaction from '../hooks/useTransaction';
+import useProductFetch from '../hooks/useProductFetch';
 
 const Dashboard = () => {
-  const {onLogOut, user, isFetching} = useUser()
+  const {onLogOut, user, isFetching} = useUser();
+  const { transaction } = useTransaction();
+  const products = useProductFetch();
 
   return (
-    <div className="container mx-auto">
-      {isFetching ? (
-        <Oval
-        height={80}
-        width={80}
-        color="#4fa94d"
-        wrapperStyle={{}}
-        wrapperClass=""
-        visible={true}
-        ariaLabel='oval-loading'
-        secondaryColor="#4fa94d"
-        strokeWidth={2}
-        strokeWidthSecondary={2}
-      
-      />
-      ) : (
-        <Fragment>
-          <div className="container mx-auto">
-            Welcome back {user.username}
-          </div>
 
-          <button
-            type='submit'
-            onClick={onLogOut}
-            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          >
-            Log Out
-          </button>
-        </Fragment>
-      )}
+  <div>
+    <Nav />
+    <div className="flex overflow-hidden bg-white pt-16">
+      <SideBar />
+      <div className="bg-gray-900 opacity-50 hidden fixed inset-0 z-10" id="sidebarBackdrop" />
+      <div id="main-content" className="h-full w-full bg-gray-50 relative overflow-y-auto lg:ml-64">
+        <main>
+          {transaction.length === 0 && products.length === 0 && isFetching ?
+            <div className='h-[30rem]'>
+              <div className="absolute right-1/2 bottom-1/2  transform translate-x-1/2 translate-y-1/2 ">
+                <div className="border-t-transparent border-solid animate-spin  rounded-full border-blue-400 border-8 h-64 w-64" />
+              </div>
+            </div>
+            : (
+              <div className="pt-6 px-4">
+                <Sales />
+                <Table transaction={transaction} user={user} products={products} />
+              </div>
+            )
+          }
+          
+        </main>
+        <Footer />
+      </div>
     </div>
+  </div>
   );
 };
 
