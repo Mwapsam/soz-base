f76c073404ffda518861e13d3d36f858a8a35718
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:41:29 +0200

    Create transaction hook

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,19 @@
 class User < ApplicationRecord
+    has_many :addresses, dependent: :destroy
+    has_many :transactions, dependent: :destroy
+    has_many :products, through: :transactions
+
     validates :username, :session_token, uniqueness: true
     validates :username, presence: true
+    validates :email, uniqueness: true
     validates :password, length: {minimum: 6, allow_nil: true}
 
     has_one_attached :photo
 
     attr_reader :password
     before_validation :ensure_session_token
+
+    scope :users_per_day, -> {  group("DATE(created_at)").count }
 
     def self.find_by_credentials(email, password)
         @user = User.find_by(email: email)
@@ -43,5 +50,17 @@ class User < ApplicationRecord
     after_create do
         customer = Stripe::Customer.create(name: username, email: email)
         update(stripe_customer_id: customer.id)
+    end
+
+    def self.customers
+        Stripe::Charge.list({limit: 7})
+    end
+
+    def self.all_customers
+        Stripe::Charge.list({limit: 100})
+    end
+
+    def total_sales
+        products.to_a.sum { |product| product.sales }
     end
 end
