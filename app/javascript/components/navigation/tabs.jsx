1370876cbc6155b88322a1b9ab688037aaad7cdf
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:26:46 +0200

    Create tabs component

---
+++ app/javascript/components/navigation/tabs.jsx
@@ -0,0 +1,123 @@
+import React, { useState } from 'react'
+import { Tab } from '@headlessui/react'
+
+function classNames(...classes) {
+    return classes.filter(Boolean).join(' ')
+  }
+  
+const MyTabs = ({description, id})=> {
+  let [categories] = useState({
+    Description: [
+      {
+        id,
+        title: description,
+        date: '5h ago',
+        commentCount: 5,
+        shareCount: 2,
+      },
+    ],
+    Details: [
+      {
+        id: 1,
+        title: 'Is tech making coffee better or worse?',
+        date: 'Jan 7',
+        commentCount: 29,
+        shareCount: 16,
+      },
+      {
+        id: 2,
+        title: 'The most innovative things happening in coffee',
+        date: 'Mar 19',
+        commentCount: 24,
+        shareCount: 12,
+      },
+    ],
+    Reviews: [
+      {
+        id: 1,
+        title: 'Ask Me Anything: 10 answers to your questions about coffee',
+        date: '2d ago',
+        commentCount: 9,
+        shareCount: 5,
+      },
+      {
+        id: 2,
+        title: "The worst advice we've ever heard about coffee",
+        date: '4d ago',
+        commentCount: 1,
+        shareCount: 2,
+      },
+    ],
+  })
+
+  return (
+    <div className="px-2 py-16 sm:px-0 m-auto">
+      <Tab.Group 
+       onChange={(index) => {
+        console.log('Changed selected tab to:', index)
+      }}
+      >
+        <Tab.List className="flex justify-center space-x-1 rounded bg-white p-1 w-full max-w-md">
+          {Object.keys(categories).map((category) => (
+            <Tab
+              key={category}
+              className={({ selected }) =>
+                classNames(
+                  'w-full rounded-lg py-2.5 text-sm font-medium leading-5',
+                  'ring-opacity-0 ring-offset-2 focus:outline-none',
+                  selected
+                    ? 'bg-white text-blue-700'
+                    : 'text-black hover:text-blue-600'
+                )
+              }
+            >
+              {category}
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels className="mt-2">
+          {Object.values(categories).map((posts, idx) => (
+            <Tab.Panel
+              key={idx}
+              className={classNames(
+                'rounded-xl bg-white p-3',
+                'ring-white ring-opacity-0 ring-offset-2 ring-offset-gray-400 focus:outline-none focus:ring-2'
+              )}
+            >
+              <ul>
+                {posts.map((post) => (
+                  <li
+                    key={post.id}
+                    className="relative rounded-md p-3 bg-gray-100"
+                  >
+                    <h3 className="text-sm font-medium leading-5">
+                      {post.title}
+                    </h3>
+
+                    <ul className="mt-1 flex space-x-1 text-xs font-normal leading-4 text-gray-500">
+                      <li>{post.date}</li>
+                      <li>&middot;</li>
+                      <li>{post.commentCount} comments</li>
+                      <li>&middot;</li>
+                      <li>{post.shareCount} shares</li>
+                    </ul>
+
+                    <a
+                      href="#"
+                      className={classNames(
+                        'absolute inset-0 rounded-md',
+                        'ring-gray-400 focus:z-10 focus:outline-none focus:ring-2'
+                      )}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </Tab.Panel>
+          ))}
+        </Tab.Panels>
+      </Tab.Group>
+    </div>
+  )
+}
+
+export default MyTabs;
