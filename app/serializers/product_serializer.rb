b0f83d90b68cdeb8bd7107fce9f11023ff81be72
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:41:53 +0200

    Create transaction hook

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,3 +1,6 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :price, :currency, :photos_urls
+  attributes :id, :name, :description, :price, :currency, :sales_count, :photos_urls
+
+  has_many :orderables
+  has_many :carts
 end
