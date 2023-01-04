24af54fb2d77a8fa20ace9266a6859b38d4fecd8
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:42:21 +0200

    Create transaction hook

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,31 @@ Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   
   resources :users, only: [:create, :index]
+  resources :transactions, only: [:index]
   resources :sessions, only: [:create, :destroy]
   resources :webhooks, only: [:create]
-  resources :products, only: [:create, :index] do
+  resources :addresses, only: [:index, :update]
+  resources :products, only: [:create, :index, :update, :destroy] do
     get :search, on: :collection
     get :photos, on: :member
+    get :admin_products, on: :collection
   end
 
   post '/create-checkout-session', to: "payments#create"
+  post '/create-address', to: 'addresses#create_address'
   post "products/add_to_cart/:id", to: "products#add_to_cart", as: "add_to_cart"
-  delete "products/remove_from_cart/:id", to: "products#remove_from_cart"
+  post "products/remove_from_cart/:id", to: "products#remove_from_cart"
+  
   get "products/get_cart", to: "products#get_cart"
-  get "success", to: "checkout#success"
-  get "cancel", to: "checkout#cancel"
+  get "get_orders", to: "products#orders"
+  get "success", to: "payments#success"
+  get "cancel", to: "payments#cancel"
+
+  get "customers", to: "transactions#get_details"
+  get "all_transactions", to: "transactions#all_transactions"
+
+  post '/increment-quantity/:id', to: "products#increment_func"
+  post '/decrement-quantity/:id', to: "products#decrease_func"
 
   get 'getuser', to: "users#getUser"
   get 'home/index'
