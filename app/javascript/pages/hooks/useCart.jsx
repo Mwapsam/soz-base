07dfd2672a2bc04be60f3f7ac7660f99690dab1f
Mwapsam <mwapsam@gmail.com>
2023-01-04 11:32:42 +0200

    Create cart hook

---
+++ app/javascript/pages/hooks/useCart.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect, useState} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getCartFunc, removeFromCartFunc, incrementFunc, decrementFunc } from '../../services/cart.service';
+
+const useCart = () => {
+    const [open, setOpen] = useState(false);
+    const cart = useSelector((state) => state.cart);
+    const dispatch = useDispatch();
+
+    const handleOpen = () => setOpen(!open);
+
+    useEffect(() => {
+        dispatch(getCartFunc());
+      }, [cart.length, dispatch]);
+
+    const handleRemoveFromCart = (product) => {
+        dispatch(removeFromCartFunc(product));
+    };
+
+    const handleDecreaseCart = (product) => {
+      dispatch(decrementFunc(product))
+    }
+  
+    const handleIncreaseCart = (product) => {
+      dispatch(incrementFunc(product))
+    }
+    
+  return {cart, open, handleOpen, handleRemoveFromCart, handleDecreaseCart, handleIncreaseCart}
+}
+
+export default useCart;
