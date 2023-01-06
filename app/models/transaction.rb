16c276d8f9d14e339622f8b209b3bcea80a5e8f4
Mwapsam <mwapsam@gmail.com>
2023-01-06 09:23:57 +0200

    Create transaction hook

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,7 @@ class Transaction < ApplicationRecord
   belongs_to :user, class_name: 'User'
   belongs_to :product
 
-  scope :sorted_transactions, ->{ order(created_at: :asc) }
+  monetize :amount_total, as: :price_cents
+
+  scope :sorted_transactions, ->{ order(created_at: :desc) }
 end
