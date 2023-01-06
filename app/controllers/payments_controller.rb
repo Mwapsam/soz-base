ba1a30fcdbd43ae7ce2f994fd25acfcec9dcd8c5
Mwapsam <mwapsam@gmail.com>
2023-01-06 09:19:47 +0200

    Create transaction hook

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,21 +1,24 @@
 class PaymentsController < ApplicationController
     def create
-      if @line_items.pluck(:currency).uniq.length > 1
-        render json: {message: "You can not select products with different currencies in one checkout"}
+      if logged_in?
+        if @line_items.pluck(:currency).uniq.length > 1
+          render json: {message: "You can not select products with different currencies in one checkout"}
+        else
+          @session = Stripe::Checkout::Session.create({
+              customer: current_user.stripe_customer_id,
+              payment_method_types: ['card'],
+              line_items: @line_items.collect {|item| item.to_builder(item.orderables[0].quantity).attributes!},
+              allow_promotion_codes: true,
+              mode: 'payment',
+              invoice_creation: {enabled: true},
+              success_url: "#{success_url}?session_id={CHECKOUT_SESSION_ID}",
+              cancel_url: cancel_url
+            })
+            render json: @session.url, status: 201
+        end
       else
-        @session = Stripe::Checkout::Session.create({
-            customer: current_user.stripe_customer_id,
-            payment_method_types: ['card'],
-            line_items: @line_items.collect {|item| item.to_builder(item.orderables[0].quantity).attributes!},
-            allow_promotion_codes: true,
-            mode: 'payment',
-            invoice_creation: {enabled: true},
-            success_url: "#{success_url}?session_id={CHECKOUT_SESSION_ID}",
-            cancel_url: cancel_url
-          })
-          render json: @session.url, status: 201
-      end
-        
+        render json: {error: "Unauthorized action!"}, status: 401
+      end  
     end
 
     def success
@@ -24,7 +27,7 @@ class PaymentsController < ApplicationController
         @session_with_expand = Stripe::Checkout::Session.retrieve({ id: params[:session_id], expand: ['line_items'] })
         @session_with_expand.line_items.data.each do |line_item|
           product = Product.find_by(stripe_product_id: line_item.price.product)
-          Transaction.create(user_id: current_user.id, product_id: product.id)
+          Transaction.create(user_id: current_user.id, product_id: product.id, amount_discount: line_item.amount_discount, amount_tax: line_item.amount_tax, amount_total: line_item.amount_total, quantity: line_item.quantity)
           product.increment!(:sales_count)
         end
       else
