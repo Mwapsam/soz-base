528c63968b95275264691c0faa94256b0625a74d
Mwapsam <mwapsam@gmail.com>
2023-01-06 09:20:35 +0200

    Create transaction hook

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { Signup, Login, Dashboard, Wrapper, Products, List, Show, Cart, Checkout, Admin, Users, Sales, AdminProducts } from '../pages/index';
+import PrivateRoute from '../components/auth/PrivateRoute';
+import AdminRoute from './auth/AdminRoute';
+import { Signup, Login, Dashboard, Wrapper, Products, List, Show, Cart, Checkout, Admin, Users, Sales, AdminProducts, Orders } from '../pages/index';
 
 const App = () => {
   const ProductsComponent = Admin(Products)
   const SalesComponent = Admin(Sales)
   const UsersComponent = Admin(Users)
   const AdminProductsComponent = Admin(AdminProducts)
+  const OrdersComponent = Admin(Orders)
   const ListComponent = Wrapper(List)
   const ShowComponent = Wrapper(Show)
   const CartComponent = Wrapper(Cart)
@@ -16,15 +19,16 @@ const App = () => {
     <Routes>
         <Route path='/register' element={<Signup/>} />
         <Route path='/login' element={<Login />} />
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/' element={<AdminRoute><Dashboard /></AdminRoute>} />
         <Route path='/products' element={<ProductsComponent/>} />
         <Route path='/products/:product' element={<ShowComponent/>} />
         <Route path='/list' element={<ListComponent/>} />
         <Route path='/cart' element={<CartComponent />} />
-        <Route path='/checkout' element={<CheckoutComponent />} />
-        <Route path='/sales' element={<SalesComponent />} />
-        <Route path='/users' element={<UsersComponent />} />
-        <Route path='/admin-products' element={<AdminProductsComponent />} />
+        <Route path='/checkout' element={<PrivateRoute><CheckoutComponent /></PrivateRoute>} />
+        <Route path='/sales' element={<AdminRoute><SalesComponent /></AdminRoute>} />
+        <Route path='/users' element={<AdminRoute><UsersComponent /></AdminRoute>} />
+        <Route path='/admin-products' element={<AdminRoute><AdminProductsComponent /></AdminRoute>} />
+        <Route path='/orders' element={<AdminRoute><OrdersComponent /></AdminRoute>} />
     </Routes>
   );
 };
