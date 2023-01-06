a3fdf763b6283e68e61fd53f4cce6468d9b0d067
Mwapsam <mwapsam@gmail.com>
2023-01-06 09:21:41 +0200

    Create transaction hook

--- app/javascript/components/navigation/Navbar.jsx
+++ app/javascript/components/navigation/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { Navbar, MobileNav, Typography, Button, IconButton , Popover, PopoverHandler, PopoverContent} from "@material-tailwind/react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
