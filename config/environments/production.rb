ba0c088fff27ad7595441a6286ac45be52a617d6
Mwapsam <mwapsam@gmail.com>
2023-01-19 14:28:28 +0200

    Deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  # config.hosts << /.*\.railway\.app/
-
-  # config.hosts = (config.hosts rescue []) << 'soz-base-production.up.railway.app'
-  config.hosts.clear
+  config.hosts = (config.hosts rescue []) << "soz-base-production.up.railway.app"
 end
