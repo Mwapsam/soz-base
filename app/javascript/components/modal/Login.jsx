610cc99fa04d6e1a880395d738db8c4e9ba3a906
Mwapsam <mwapsam@gmail.com>
2023-04-06 11:42:01 +0200

    Fix errors

--- app/javascript/components/modal/Login.jsx
+++ app/javascript/components/modal/Login.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import {
     Button,
